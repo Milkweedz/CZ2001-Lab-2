6f9b99e5719847a8cedc1954697b1fa2e765e630
oysterly <37858934+oysterly@users.noreply.github.com>
2018-09-15 23:40:10 +0800

    Update HashGenerator.java

--- src/HashGenerator.java
+++ src/HashGenerator.java
@@ -29,6 +29,74 @@ public class HashGenerator {
     }
 
     public static void Hash(File file){
+        int size = 11;
+        int index = 0;
+        int i = 0;
+        //this is the size of the hash table - a prime number is best
+        String[] hashTable = new String[size];
+        HashMap = new HashBucket[size];
+        //create the hash table
+        LinkedList<String> temp = new LinkedList<String>();
 
+        //fill the hash table so that every slot contains a space
+        getContents(file);
+        for (i = 0; i < 104; i++) {
+            if (!array[i].trim().equals("{") && !array[i].trim().equals("},")) {
+                if (array[i].trim().substring(1,7).equals("POSTAL")) {
+                    index = i % size;
+                    //hashTable[index] = array[i].trim().substring(11,17);
+                    System.out.println(array[i].trim().substring(11,17));
+                    HashMap[index] = new HashBucket(Integer.parseInt(array[i].trim().substring(11,17)),temp);
+                } else 
+                    temp.add(array[i]);
+            }
+                    
+        }
+        i = 0;
+        while (HashMap[i] != null) {
+            System.out.println(HashMap[i].getKey());
+            i++;
+        }
     }
+    
+    public static String getContents(File aFile) {
+        //...checks on aFile are elided
+        StringBuffer contents = new StringBuffer();
+
+        //declared here only to make visible to finally clause
+        BufferedReader input = null;
+        try {
+        //use buffering, reading one line at a time
+        //FileReader always assumes default encoding is OK!
+        input = new BufferedReader( new FileReader(aFile) );
+        String line = null; //not declared within while loop
+        /*
+        * readLine is a bit quirky :
+        * it returns the content of a line MINUS the newline.
+        * it returns null only for the END of the stream.
+        * it returns an empty String if two newlines appear in a row.
+        */
+        int i = 0;
+        while (( line = input.readLine()) != null){
+            array[i]=line;
+            i++;
+            contents.append(System.getProperty("line.separator"));
+        }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+        finally {
+        try {
+            if (input!= null) {
+            //flush and close both "input" and its underlying FileReader
+            input.close();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        }
+    return contents.toString();
+  }
 }
